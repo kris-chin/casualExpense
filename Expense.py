35ae6a0d40933a41d4cd9e40e6cb50c407a178b3
Krikrifreeze <krikrifreeze@gmail.com>
2019-09-06 17:24:44 -0700

    Added some labels

--- Expense.py
+++ Expense.py
@@ -9,9 +9,13 @@
 '''
 
 class Expense():
-    def __init__(self,name,cost,rate=1):
-        self.name = name
-        self.cost = cost
-        self.rate = rate
-        
-        self.count = 0 #the number of times the purchase is made
+    def __init__(self,name,cost,rate=1,spread=(0,0)):
+        self.name = name #name of the expense
+        self.cost = cost #exact price of the expense
+        self.rate = rate #how many days before you make the expense again
+        self.spread = spread #how much to vary each purchase every time its made. first element is lowest, second element is highest (simulates buying different lunches in roughly the same range) defaults to 0
+
+        #plot stuff
+        self.count = 0 #the number of times the purchase has been made
+        self.cumCost = 0
+        self.plot = [] #plot of the value over time
--- main.py
+++ main.py
@@ -3,8 +3,10 @@
     Author: Krischin Layon
 
     Graphs the money bleeding from your wallet.
-
     Each recurring expense is represented as an expense object that contains a name, initial cost, and recurrance rate
+
+    #TODO: add easy identification of amounts after certain days
+    #TODO: add a way to adjust the paramaters of each expense to see how to fix your budget
 
     main.py
 
@@ -12,36 +14,49 @@
 '''
 
 from matplotlib import pyplot as plt
+from matplotlib import ticker as tick
 import numpy as np
+import random
 
 from Expense import *
 
 def main():
     expenses = [
-        Expense("Coffee ($2.45)",2.45,1),
-        Expense("Gas ($20)", 20, 3),
-        Expense("Mom Money (-$100)", -100, 7)
+        Expense("Coffee",2.45,1),
+        Expense("Gas", 20, 4),
+        Expense("Lunch",6.59,1,spread=(0,4))
     ]
 
     #start pyplot
     fig = plt.figure()
     ax = plt.axes()
 
-    days = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14]
+    days = np.linspace(0,14,15)
     totalCost = 0
-    totalCostPlot = []
+    totalCostPlot = [] #y's of the total cost at a particular point in time
 
     for d in days:
         for e in expenses: #go through every expense we listed
             if d%e.rate == 0: #if its time to add another purchase
-                totalCost += (e.count*e.cost)
-                e.count+= 1
+                if e.spread != (0,0):
+                    randValue = random.uniform(-e.spread[0],e.spread[1]) #calculate the variance in the purchase
+                else:
+                    randValue = 0
+                totalCost += (e.cost + randValue)
+                e.cumCost += (e.cost + randValue)
+                e.count+=1
+            e.plot.append(e.cumCost)
+
         totalCostPlot.append(totalCost)
     
-    plt.plot(days,totalCostPlot,label = "Total Expenses")
+    plt.plot(days,totalCostPlot,label = "Total Expenses",color = "black")
+
+    #plot expenses
+    for e in expenses: plt.plot(days,e.plot,label=(e.name + ": $" + str(e.cost) + " ±\$(" + str(e.spread[0]) + ", " + str(e.spread[1]) + ") every ~" + str(e.rate) +" days") )
 
     #ticks
     plt.xticks(days)
+    ax.yaxis.set_major_formatter(tick.StrMethodFormatter('${x:,.0f}')) #add the dollar signs to the y axis
     ax.legend()
     #labels
     plt.title("The cost of your actions")
