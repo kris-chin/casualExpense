f25aca58e5b89ec4a918456673c9eccfafe3788e
Krikrifreeze <krikrifreeze@gmail.com>
2019-10-15 17:41:11 -0700

    Configured main.js properly with electron and added basic text to index.html

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
-const { app, BrowserWindow } = require('electron');
+const {app,BrowserWindow} = require('electron');
 
-function createWindow()
+function createWindow(){
+    //creates the browser window
+    let win = new BrowserWindow({
+        width: 800, //width of the window
+        height: 600, //height of the window
+        webPreferences:{
+            nodeIntegration: true //integrate Node.JS
+        }
+    })
+
+    //load the app's index.html
+    win.loadFile('index.html')
+}
+
+app.on('ready',createWindow) //load the window. on() being a method of BrowserWindow
